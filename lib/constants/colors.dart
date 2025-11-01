727ffda2654f19079878779fe0a38bfa8e05fdea
Adepoju Opeyemi <opepoju@gmail.com>
2025-11-01 17:24:31 +0100

    feat: setup colors  for easy config

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color orange = Color(0xFFFFA726);
+  static const Color pink = Color(0xFFEC0C43);
+  static const Color blackPrimary = Color(0xFF575757);
+  static const Color blackSecondary = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color textGray = Color(0x00808080);
+}
